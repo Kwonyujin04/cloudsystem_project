ff657c7022dfe2f43b503831d0d3a41b3911042b
Kwonyujin04 <2310867@sookmyung.ac.kr>
2025-11-17 23:43:45 +0900

    Feat: Tailwind css 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyPage from "./pages/MyPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
